51db78b8e46d6c4d231bba89884a056c1f37f874
vsk <m-vsk@yandex.ru>
2018-05-03 20:03:36 +0300

    Update code

--- WhileMultiples10.js
+++ WhileMultiples10.js
@@ -1,3 +1,9 @@
-for (var i = 45; i <= 670; i++) {
-    if (i % 10 == 0) console.log(i);
+//for (var i = 45; i <= 670; i++) {
+//    if (i % 10 == 0) console.log(i);
+//}
+
+var i = 45;
+while (i <= 670) {
+    if (i % 10 === 0) console.log(i);
+    i++;
 }
