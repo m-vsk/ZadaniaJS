8193efe7415e2302f12a4656719f8cfff8f2529b
vsk <m-vsk@yandex.ru>
2018-05-03 20:23:35 +0300

    Update code

---
+++ Tovar.js
@@ -0,0 +1,11 @@
+function words(num) {
+    var titles = ['товар', 'товара', 'товаров'];
+    var cases = [2, 0, 1, 1, 1, 2];
+    return num + ' ' + titles[(num % 100 > 4 && num % 100 < 20) ? 2 : cases[Math.min(num % 10, 5)]];
+}
+
+console.log(words(0)); //0 товаров
+console.log(words(1)); //1 товар
+console.log(words(100)); //100 товаров
+console.log(words(12)); //12 товаров
+console.log(words(22)); //22 товара
